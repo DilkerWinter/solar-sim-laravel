417ec2f557f7f605838dbfbbc38fb7f710fa1bc9
Bruno Winter <brunohdwinter@gmail.com>
2025-05-23 03:42:53 -0400

    fix: tentando arrumar o erro de CI com as interfaces de Edit.jsx do product

--- resources/js/Pages/Products/Edit.jsx
+++ resources/js/Pages/Products/Edit.jsx
@@ -1,1 +1,10 @@
-<p>Teste</p>
+import React from 'react';
+
+export default function Edit({ product }) {
+  return (
+    <div>
+      <p>Teste</p>
+      <h2>Editando o produto: {product.name}</h2>
+    </div>
+  );
+}
