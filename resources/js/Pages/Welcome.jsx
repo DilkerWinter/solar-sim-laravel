7de1538cfa6991f31be45814b8be9194813db991
Bruno Winter <brunohdwinter@gmail.com>
2025-05-25 22:51:34 -0400

    test: alterando texto site

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -214,7 +214,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
 
                                     <div className="pt-3 sm:pt-5">
                                         <h2 className="text-xl font-semibold text-black dark:text-white">
-                                            Laravel News
+                                            Casa de alugel teste
                                         </h2>
 
                                         <p className="mt-4 text-sm/relaxed">
