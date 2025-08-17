d61d504e7594404a1dbf13a77a302573c1bbd411
Bruno Winter <brunohdwinter@gmail.com>
2025-08-17 14:57:10 -0400

    feat: add submit button

--- resources/js/Pages/Products/Create.jsx
+++ resources/js/Pages/Products/Create.jsx
@@ -1,4 +1,5 @@
 import CustomBreadcrumb from '@/Components/AppLayout/CustomBreadcrumb';
+import SubmitButton from '@/Components/Customer/Create/SubmitButton';
 import CreateBaseProduct from '@/Components/Product/Sections/CreateBaseProduct';
 import CreateExtraProduct from '@/Components/Product/Sections/CreateExtraProduct';
 import CreteProductHeader from '@/Components/Product/Sections/CreteProductHeader';
@@ -48,12 +49,8 @@ export default function Create( { productTypes } ) {
         )}
 
         <div className="flex justify-end">
-          <button
-            type="submit"
-            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-          >
-            Criar Produto
-          </button>
+          <SubmitButton text={"Cadastrar"}onSubmit={handleSubmit}>
+          </SubmitButton>
         </div>
 
       </form>
