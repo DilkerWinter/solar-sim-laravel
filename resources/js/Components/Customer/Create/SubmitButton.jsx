06559eecc7276469796033327212a72c6da02c4f
Bruno Winter <brunohdwinter@gmail.com>
2025-08-23 15:13:29 -0400

    refactor: alterando cor do botao do create e removendo margin

--- resources/js/Components/Customer/Create/SubmitButton.jsx
+++ resources/js/Components/Customer/Create/SubmitButton.jsx
@@ -4,7 +4,7 @@ export default function SubmitButton({ text, onClick, onSubmit }) {
             type="submit"
             onClick={onClick}
             onSubmit={onSubmit}
-            className="bg-blue-600 text-white px-6 py-2 rounded-2xl hover:bg-blue-700  text-xl m-12"
+            className="bg-green-600 text-white px-6 py-2 rounded-2xl hover:bg-green-700  text-xl "
         >
             {text}
         </button>
