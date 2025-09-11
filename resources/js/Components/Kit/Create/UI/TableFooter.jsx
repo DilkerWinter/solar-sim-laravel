429fb84ab456c3ff6dfe4da47d88b01293dcf188
Bruno Winter <brunohdwinter@gmail.com>
2025-09-11 18:58:09 -0400

    feat: mostrar resumo do kit

--- resources/js/Components/Kit/Create/UI/TableFooter.jsx
+++ resources/js/Components/Kit/Create/UI/TableFooter.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function TableFooter({ products }) {
     const [totalPrice, setTotalPrice] = useState("0,00");
     const [totalKwh, setTotalKwh] = useState(0);
+    const [supportedKwh, setSupportedKwh] = useState(0);
 
     const parsePrice = (priceString) => {
         if (typeof priceString === "number") return priceString;
@@ -10,7 +11,7 @@ export default function TableFooter({ products }) {
         return Number(priceString.replace(/\./g, "").replace(",", "."));
     };
 
-    useEffect(() => {
+    function calculatePrice() {
         const total = products.reduce((acc, product) => {
             const quantity = Number(product.quantity) || 0;
             const price = parsePrice(product.price);
@@ -24,19 +25,50 @@ export default function TableFooter({ products }) {
                 minimumFractionDigits: 2,
             })
         );
+    }
+
+    function calculateTotalPower() {
+        const total = products.reduce((acc, product) => {
+            if (product.solarPanel) {
+                const quantity = Number(product.quantity) || 0;
+                const energyPerDay = product.solarPanel.average_daily_energy_wh || 0;
+                return acc + (energyPerDay * quantity) / 1000;
+            }
+            return acc;
+        }, 0);
+
+        setTotalKwh(total);
+    }
+
+    function calculateSupportedPower() {
+        const total = products.reduce((acc, product) => {
+            if (product.inverter) {
+                const quantity = Number(product.quantity) || 0;
+                const maxPower = product.inverter.max_power_watts || 0;
+                return acc + (maxPower * quantity) / 1000;
+            }
+            return acc;
+        }, 0);
+
+        setSupportedKwh(total);
+    }
+
+    useEffect(() => {
+        calculatePrice();
+        calculateTotalPower();
+        calculateSupportedPower();
     }, [products]);
 
     return (
         <div className="border-t border-gray-200 px-3 py-2 bg-gray-100">
-            <div className="flex items-center justify-between">
-                <div>
-                    <span className="text-sm font-medium text-gray-900">
-                        KhW Gerados: {totalKwh}
-                    </span>
+            <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
+                <div className="flex flex-col  text-gray-900">
+                    <span>kWh Gerados/dia: {totalKwh.toFixed(2)} kWh</span>
+                    <span>Capacidade Inversores: {supportedKwh.toFixed(2)} kW</span>
                 </div>
 
                 <div className="flex items-center gap-1 text-sm">
-                    <span className="text-sm font-medium text-gray-900">
+                    <span className="text-lg font-medium text-gray-900">
                         Preço Total: {totalPrice}
                     </span>
                 </div>
