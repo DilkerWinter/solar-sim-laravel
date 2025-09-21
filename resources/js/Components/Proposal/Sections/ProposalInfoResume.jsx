a831d661d560ce3c2fe58cfb91a73a17e4d9059a
DilkerWinter <brunohdwinter@gmail.com>
2025-09-21 12:49:45 -0400

    feat: icons

--- resources/js/Components/Proposal/Sections/ProposalInfoResume.jsx
+++ resources/js/Components/Proposal/Sections/ProposalInfoResume.jsx
@@ -1,21 +1,21 @@
-import InfoResumeCard from "@/Components/UI/Cards/CustomerInfoCard";
-import { Building, Clock, File, FileBarChart, List, ListCollapse, Package, Sun, Users, Zap } from "lucide-react";
+import InfoResumeCard from "@/Components/UI/Cards/InfoResumeCard";
+import { X, Check } from "lucide-react";
 
 export default function ProposalInfoResume() {
     return (
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-6 mb-8">
+        <div className="flex flex-col sm:flex-row sm:items-center gap-6 mb-8">
             <InfoResumeCard
                 label="Propostas Abertas"
                 url={"/proposals/count/open"}
-                icon={Package}
-                colorClass="text-blue-600"
+                icon={Check}
+                colorClass="text-green-600"
             />
 
             <InfoResumeCard
                 label="Propostas Fechadas"
                 url={"/proposals/count/closed"}
-                icon={Package}
-                colorClass="text-blue-600"
+                icon={X}
+                colorClass="text-red-600"
             />
 
         </div>
