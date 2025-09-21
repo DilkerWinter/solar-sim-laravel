3938c28ee1b4011caf835a8d098d38ba3a854382
bruno <bruno@winter.com>
2025-09-21 12:19:50 -0400

    feat: create header

--- resources/js/Pages/Proposals/Index.jsx
+++ resources/js/Pages/Proposals/Index.jsx
@@ -1,6 +1,22 @@
+import CustomBreadcrumb from "@/Components/AppLayout/CustomBreadcrumb";
+import KitInfoResume from "@/Components/Kit/Index/Sections/KitInfoResume";
+import ProposalIndexHeader from "@/Components/Proposal/Sections/ProposalIndexHeader";
+import AppLayout from "@/Layouts/AppLayout";
+
 export default function Index() {
     return (
-        <>
-        </>
+        <div className="w-full mx-auto p-8">
+            <ProposalIndexHeader />
+        </div>
     );
 }
+
+Index.layout = (page) => (
+    <AppLayout breadcrumb={<CustomBreadcrumb
+        items={[
+            { name: "Início", href: "/dashboard" },
+            { name: "Propostas" },
+        ]}
+    />
+    }>{page}</AppLayout>
+);
